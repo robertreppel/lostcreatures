1475947f390d5990e18c4acc8cf8a5486ff7a00a
Robert Reppel <rreppel@gmail.com>
2015-09-22 06:32:26 -0700

    Download all lost animals in Vancouver.
    Index them in Elasticsearch

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM elasticsearch:latest
+
+
---
+++ download-lostanimals.py
@@ -0,0 +1,22 @@
+
+# Import all lost animals in Vancouver into Elasticsearch
+ 
+import urllib
+import json
+from pprint import pprint
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+# https://elasticsearch-py.readthedocs.org/en/master/
+
+lostAnimalsJson = urllib.urlopen('ftp://webftp.vancouver.ca/OpenData/json/LostAnimals.json')
+lostAnimalsJsonArray = json.load(lostAnimalsJson)
+
+es = Elasticsearch("http://172.17.0.2")
+animalCount = 0
+for i in lostAnimalsJsonArray:
+    print i["Name"]
+    animalCount = animalCount + 1
+    res = es.index(index="animals", doc_type="lostanimal", body=i)
+    print(res['created'])
+print animalCount
